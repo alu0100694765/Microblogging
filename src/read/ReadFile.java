273e8f7c777883cbd84a482c62c8fa189534c594
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2015-12-03 11:24:28 +0000

    Added ReadFile class

---
+++ src/read/ReadFile.java
@@ -0,0 +1,37 @@
+/**
+ * Author: Sawan J. Kapai Harpalani
+ * Email: sawankapai@gmail.com
+ * File name: ReadFile.java
+ * Version: 1.0
+ * Date: 03/12/2015
+ * Description:
+ * Copyright: Copyright 200X Sawan J. Kapai Harpalani
+ *			 
+ *			 This file is part of Math Attack.
+ *
+ *			 MicroBlogging is free software: you can redistribute it 
+ *			 and/or modify it under the terms of the GNU General	
+ *   		 Public License as published by the Free Software 
+ *		     Foundation, either version 3 of the License,
+ *			 or (at your option) any later version.
+ *
+ *
+ *			 MicroBlogging is distributed in the hope that it will 
+ *			 be useful, but WITHOUT ANY WARRANTY; without even 
+ *			 the implied warranty of MERCHANTABILITY or FITNESS
+ *			 FOR A PARTICULAR PURPOSE. See the GNU General Public
+ *			 License for more details.
+ *			
+ *			 You should have received a copy of the GNU General
+ *			 Public License along with MicroBlogging. If not, see
+ *			 http://www.gnu.org/licenses/.
+ */
+package read;
+
+/**
+ * @author Sawan J. Kapai Harpalani
+ *
+ */
+public class ReadFile {
+
+}
