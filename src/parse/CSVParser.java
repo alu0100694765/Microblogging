6c14faae38ece844bc7e5d87170ae00db42207dd
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2015-12-03 16:07:42 +0000

    Building parser going to implement error exception for field

--- src/parse/CSVParser.java
+++ src/parse/CSVParser.java
@@ -28,16 +28,83 @@
  */
 package parse;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import document.Document;
+import errors.FieldException;
 
 /**
  * @author Sawan J. Kapai Harpalani
- *
+ * 
  */
 public class CSVParser {
 
-//	public static ArrayList<> parse(ArrayList<String> content) {
-//		return null;
-//	}
+	protected final static String CSV_TOKEN = ",";
+	protected final static int ID_FIELD = 0;
+	protected final static int ID_MEMBER_FIELD = 1;
+	protected final static int TIMESTAMP_FIELD = 2;
+	protected final static int TEXT_FIELD = 3;
+	protected final static int GEO_LAT_FIELD = 4;
+	protected final static int GEO_LNG_FIELD = 5;
+
+	public static ArrayList<Document> parse(ArrayList<String> content) throws FieldException {
+		ArrayList<Document> parsedContent =  new ArrayList<Document>();
+		
+		for (Iterator iterator = content.iterator(); iterator.hasNext();) {
+			String contentLine = (String) iterator.next();
+			
+			StringTokenizer tokenizer = new StringTokenizer(CSV_TOKEN);
+			
+			int currentField = 0;
+			
+			// Document values
+			Long id = 0L;
+			Long idMember = 0L;
+			Timestamp timestamp = null;
+			String text = null;
+			Double geoLat = 0D;
+			Double geoLng = 0D;
+			
+			while (tokenizer.hasMoreTokens()) {
+				String field = tokenizer.nextToken();
+				
+				switch (currentField) {
+					case ID_FIELD:
+						id = Long.parseLong(field);
+						break;
+					
+					case ID_MEMBER_FIELD:
+						idMember = Long.parseLong(field);
+						break;
+					
+					case TIMESTAMP_FIELD:
+						timestamp = Timestamp.valueOf(field);
+						break;
+					
+					case TEXT_FIELD:
+						text = field;
+						break;
+						
+					case GEO_LAT_FIELD:
+						geoLat = Double.parseDouble(field);
+						break;
+						
+					case GEO_LNG_FIELD:
+						geoLng = Double.parseDouble(field);
+						break;
+						
+					default:
+						throw new FieldException();
+				}
+				
+			}
+		}
+		
+		return parsedContent;
+	}
 	
+
 }
